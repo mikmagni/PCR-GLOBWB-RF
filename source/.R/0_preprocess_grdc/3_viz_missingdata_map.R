face19b305d05c786027b2e4fc7961223c74f349
Mike Magni <6574882@velocity.geo.uu.nl>
2022-08-21 17:21:57 +0200

    fixed missing data plot

--- source/.R/0_preprocess_grdc/3_viz_missingdata_map.R
+++ source/.R/0_preprocess_grdc/3_viz_missingdata_map.R
@@ -3,7 +3,7 @@ source('../fun_0_loadLibrary.R')
 ####-------------------------------####
 
 stationInfo <- read.csv('../../../data/stationLatLon.csv') %>%
-  select(., grdc_no, lon, lat, miss) %>% 
+  select(., grdc_no, lon, lat, miss, area) %>% 
   mutate(available=100-miss)
 
 outputDir <- paste0('../../../RF/viz/')
@@ -14,7 +14,7 @@ stations_xy <- stationInfo %>% select(grdc_no, lat, lon)
 
 myPalette <- colorRampPalette((brewer.pal(9, "RdYlBu")))
 sc <- scale_fill_gradientn(colours = myPalette(100), limits=c(0,100), 
-                           name='Available data (%)\n')
+                           breaks=c(0,50,100), name='Available data (%)')
 
 missing_map <- ggplot() +
   geom_map(
@@ -25,17 +25,24 @@ missing_map <- ggplot() +
   theme_map()+
   xlim(-180,180)+
   ylim(-55,75)+
-  geom_point(stationInfo, mapping = aes(x = lon, y = lat,  fill=available),
-             color='black', pch=21, size = 2) +
+  geom_point(stationInfo, mapping = aes(x = lon, y = lat,  fill=available,  size = area),
+             color='black', pch=21, alpha=0.8) +
   sc+
   theme(legend.title = element_text(size=18),
-        legend.text = element_text(size = 12),
-        # legend.position="bottom",
-        legend.position = c(0.5,-0.025), legend.direction = "horizontal",
+        legend.text = element_text(size = 14),
+        legend.direction = 'horizontal',
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         axis.ticks = element_blank(),
-        panel.grid = element_blank())
+        panel.grid = element_blank())+
+  scale_size(name=expression(paste("Upstream area ", "(km"^"2",")")),
+             breaks=c(10000,100000,500000,1000000,4680000),
+             labels=c('asd','10 000 < A < 100 000',
+                      '100 000 < A < 500 000', '500 000 < A < 1 000 000 ',
+                      '1 000 000 < A < 4 680 000'),
+             range=c(2,8))+
+  guides(size=guide_legend(direction='vertical'))
+  
 missing_map
 
 
